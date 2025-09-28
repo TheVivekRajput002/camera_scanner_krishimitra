3dfc2e7c293c7c34e704faff956e1e04065fbea1
Vivek Rajput <work.tvivekrajput@gmail.com>
2025-09-28 18:54:49 +0530

    haa maybeee ab

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // ✅ Correct import for Tailwind v4
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss() // ✅ Use as a Vite plugin
-  ],
+  plugins: [react(), tailwindcss()],
 })
